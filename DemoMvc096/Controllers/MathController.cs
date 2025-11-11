f785ba05b1c7c66a38c1601781d7d54b6caff991
Nasper01 <jamedaniel5747@gmail.com>
2025-11-11 11:03:25 +0700

    Buổi 5

--- DemoMvc096/Controllers/MathController.cs
+++ DemoMvc096/Controllers/MathController.cs
@@ -28,26 +28,7 @@ namespace DemoMvc096.Controllers
                     model.Result = $"Phương trình có 1 nghiệm: x = {x}";
                 }
             }
-            else
-            {
-                double delta = model.B * model.B - 4 * model.A * model.C;
 
-                if (delta < 0)
-                {
-                    model.Result = "Phương trình vô nghiệm";
-                }
-                else if (delta == 0)
-                {
-                    double x = -model.B / (2 * model.A);
-                    model.Result = $"Phương trình có nghiệm kép: x₁ = x₂ = {x}";
-                }
-                else
-                {
-                    double x1 = (-model.B + Math.Sqrt(delta)) / (2 * model.A);
-                    double x2 = (-model.B - Math.Sqrt(delta)) / (2 * model.A);
-                    model.Result = $"Phương trình có 2 nghiệm phân biệt: x₁ = {x1}, x₂ = {x2}";
-                }
-            }
 
             return View(model);
         }
